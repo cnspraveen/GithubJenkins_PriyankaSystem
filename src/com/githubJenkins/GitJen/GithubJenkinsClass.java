87751a4f33613b1cc9e5444f42114c6a65e4a322
cnspraveen <cns.praveen@gmail.com>
2016-08-17 12:34:42 +0530

    Update GithubJenkinsClass.java

--- src/com/githubJenkins/GitJen/GithubJenkinsClass.java
+++ src/com/githubJenkins/GitJen/GithubJenkinsClass.java
@@ -13,12 +13,14 @@ public class GithubJenkinsClass {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://mail.google.com");
 		Reporter.log("Opening Gmail in firefox", true);
+		Reporter.log("Changes done in Github", true);
 		//Reporter.log("DynamicElements", true);
 		
 		System.setProperty("webdriver.chrome.driver", "D:/PraveenBackUp/EclipseAdvanceSeleniumPractiseClass/Jarfiles/chromedriver.exe");
 	    WebDriver driver2 = new ChromeDriver();
 	    driver2.navigate().to("https://github.com/");
 	    Reporter.log("Opening Github in Chrome", true);
+	    Reporter.log("Changes done in Github", true);
 	    driver.close();
 	    driver2.close();
 	}
