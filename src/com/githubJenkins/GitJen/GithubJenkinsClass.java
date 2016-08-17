7aa02fdb24d3e05b331d1a0c99c6ff0d4b35cbd9
pmagdum <pmagdum@gmail.com>
2016-08-17 12:33:16 +0530

    Moving into github

---
+++ src/com/githubJenkins/GitJen/GithubJenkinsClass.java
@@ -0,0 +1,26 @@
+package com.githubJenkins.GitJen;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GithubJenkinsClass {
+	@Test
+	public void DynamicElements()
+	{
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://mail.google.com");
+		Reporter.log("Opening Gmail in firefox", true);
+		//Reporter.log("DynamicElements", true);
+		
+		System.setProperty("webdriver.chrome.driver", "D:/PraveenBackUp/EclipseAdvanceSeleniumPractiseClass/Jarfiles/chromedriver.exe");
+	    WebDriver driver2 = new ChromeDriver();
+	    driver2.navigate().to("https://github.com/");
+	    Reporter.log("Opening Github in Chrome", true);
+	    driver.close();
+	    driver2.close();
+	}
+
+}
